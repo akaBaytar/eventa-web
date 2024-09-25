e8b7417ae5fb4263b03a9d488b1eff5f31df0662
Burak Bilgili <burak.bilgili@icloud.com>
2024-09-25 18:23:52 +0300

    visual adjustments - padding

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -16,14 +16,14 @@ const EventDetailPage = async ({ params: { id } }: PropTypes) => {
 
   if (event) {
     return (
-      <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
-        <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
+      <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain py-16'>
+        <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl wrapper'>
           <Image
             src={event.imageUrl}
             alt='Event hero image'
             width={1000}
             height={1000}
-            className='h-full min-h-[18.75rem] object-cover object-center'
+            className='h-full min-h-[18.75rem] object-cover object-center rounded-lg'
           />
           <div className='flex flex-col gap-8 p-6 md:p-12 w-full'>
             <div className='flex flex-col gap-6'>
@@ -34,7 +34,7 @@ const EventDetailPage = async ({ params: { id } }: PropTypes) => {
                     {event.isFree ? 'Free' : `$${event.price}`}
                   </p>
                   <p className='flex items-center p-medium-16 text-gray-500 rounded-lg bg-gray-500/10 px-4 py-2'>
-                    {event.category.name}
+                    {event.category?.name}
                   </p>
                 </div>
                 <p className='p-medium-18 ms-2 mt-2 md:mt-0'>
